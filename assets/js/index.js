d444e34dae010b6b6e8fa18279410ef8751aceac
Sixtus32 <sixtuswork18@gmail.com>
2022-05-02 13:28:34 +0200

    Fixed-width canvas

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,8 +4,8 @@ const canvas = document.querySelector('canvas');
 const score = document.getElementById('scoreElement');
 const c = canvas.getContext('2d');
 
-canvas.width = innerWidth;
-canvas.height = innerHeight;
+canvas.width = 1024;
+canvas.height = 576;
 
 
 class Player {
